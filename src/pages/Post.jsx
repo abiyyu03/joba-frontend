a8877b37c0293f8818ad5167c4244abd273ddb10
abiyyu03 <abiyyu652@gmail.com>
2023-12-08 00:51:29 +0700

    create post page

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,9 +1,11 @@
 import PostCard from '../components/PostCard'
+import BottomNav from '../components/BottomNav'
 
 const Post = () => {
     return (
         <div className="container">
             <PostCard />
+            <BottomNav />
         </div>
     )
 }
