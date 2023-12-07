9e4d2746e28a1c4bf80b6debf290f8083559d776
abiyyu03 <abiyyu652@gmail.com>
2023-12-08 00:50:57 +0700

    add register and login in app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router';
 import './index.css';
 import Post from './pages/Post';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
     return (
         <Routes>
             {/* <Route path='/' element={<Home />} /> */}
             <Route path='/posts' element={<Post />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
         </Routes>
     )
 }
