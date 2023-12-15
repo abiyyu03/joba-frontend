7ff84fccd2fdb2b7962ef44c999e2ecaa58646ef
Aliffathan.0 <fathanaif34@gmai.com>
2023-12-15 12:41:55 +0700

    navbar update

--- src/components/BottomNav/index.jsx
+++ src/components/BottomNav/index.jsx
@@ -1,12 +1,24 @@
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserCircle, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faUserCircle, faHome, faBriefcase, faMagnifyingGlass, faFileCircleCheck, faBookmark } from "@fortawesome/free-solid-svg-icons";
 const BottomNav = () => {
     return (
         <div className="btm-nav">
             <button>
-                <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
-                <p>Home</p>
+                <FontAwesomeIcon icon={faBriefcase}></FontAwesomeIcon>
+                <p>Find Job</p>
+            </button>
+            <button>
+            <FontAwesomeIcon icon={faFileCircleCheck}></FontAwesomeIcon>
+                <p>Create Job</p>
+            </button>
+            <button>
+            <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+                <p>Search</p>
+            </button>
+            <button>
+            <FontAwesomeIcon icon={faBookmark}></FontAwesomeIcon>
+                <p>Bookmark</p>
             </button>
             <button>
                 <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
